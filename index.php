7ba919e30fb7541b62b25850ea59e097dc01a4d1
Fabio Mora <mail@fabio.mora.name>
2013-07-05 17:00:49 +0200

    works on Chrome 28

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Player</title>
+    <title>TouchPlayer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="/vendor/js-cover-flow/coverflow.css" rel="stylesheet" media="screen">
-    <link href="/css/base.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/js-cover-flow/coverflow.css" rel="stylesheet" media="screen">
+    <link href="css/base.css" rel="stylesheet" media="screen">
   </head>
+
   <body>
-    <div class="content">
-    	<div id="container"></div>
+    <div class="screen-size">
+    	<div id="coverflow"></div>
     </div>
 
-    <script src="/vendor/jquery/jquery.min.js"></script>
-    <script src="/vendor/js-cover-flow/coverflow.js"></script>
-    <script src="/main.js"></script>
+    <audio id="player" src="" preload loop autoplay></audio>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/js-cover-flow/coverflow.js"></script>
+    <script src="vendor/html5media.min.js"></script>
+    <script src="js/main.js"></script>
   </body>
 </html>
+
+
