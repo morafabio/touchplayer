78c72d2206de536363bad7041bea1d8915402869
Fabio Mora <mail@fabio.mora.name>
2013-07-05 14:36:38 +0200

    initial commit with libs

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Player</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/vendor/js-cover-flow/coverflow.css" rel="stylesheet" media="screen">
+    <link href="/css/base.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <div class="content">
+    	<div id="container"></div>
+    </div>
+
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/js-cover-flow/coverflow.js"></script>
+    <script src="/main.js"></script>
+  </body>
+</html>
